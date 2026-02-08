7419c438900363978013723ba3f2ffb9cb4fbbf0
Ravi-5689 <ravibhaichavda4653@gmail.com>
2026-02-08 18:22:09 +0530

    Completed Django Polls app till Part 3: models, admin, views, templates, and question listing

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,7 +1,7 @@
-from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("polls/", include("polls.urls")),
+    path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail'),
 ]
